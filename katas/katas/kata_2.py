1f2151e9d9a9c7ddcff609eda17b573e17bdc12b
Tomasz Nowakowski <toalak@gmail.com>
2024-03-17 17:08:40 +0100

    Kata 2: make the test pass; refactoring is not necessary right now

--- katas/katas/kata_2.py
+++ katas/katas/kata_2.py
@@ -8,3 +8,7 @@ class Calculator:
             return 0
         elif exprstr.isdigit() or (exprstr[1:].isdigit() and exprstr[0] == "-"):
             return int(exprstr)
+        else:
+            str_values = exprstr.split(",")
+            int_values = [int(x) for x in str_values]
+            return sum(int_values)
