7c24a1697fb40d19a90d3ccd144f9d3113536c59
Tomasz Nowakowski <toalak@gmail.com>
2024-03-17 16:41:30 +0100

    Use Kata 1 as a part of the script.

--- katas/main.py
+++ katas/main.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
+from katas import kata_1
+
+
 if __name__ == "__main__":
-    print("Hello World!")
+    fizz_buzz_solver = kata_1.FizzBuzzSolver()
+    print("Kata 1:")
+    print("Fizz Buzz of 35")
+    print(fizz_buzz_solver.solve(35))
+    print()
