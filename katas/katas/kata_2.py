9710ac27df11ff2cb33aa85083c28bdda7a034c2
Tomasz Nowakowski <toalak@gmail.com>
2024-03-17 17:30:30 +0100

    Kata 2: refactor

--- katas/katas/kata_2.py
+++ katas/katas/kata_2.py
@@ -3,17 +3,8 @@
 
 class Calculator:
     @staticmethod
-    def add(exprstr, *args):
-        if not args:
-            if exprstr == "":
-                return 0
-            elif exprstr.isdigit() or (exprstr[1:].isdigit() and exprstr[0] == "-"):
-                return int(exprstr)
-            else:
-                str_values = exprstr.split(",")
-                int_values = [int(x) for x in str_values]
-                return sum(int_values)
-        str_values = exprstr.split(",")
+    def add(*args):
+        str_values = []
         for arg in args:
             if arg == "":
                 str_values.append("0")
