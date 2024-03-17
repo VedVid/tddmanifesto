b88d8b0c2d54c6f4271f72926c61e168c1c77348
Tomasz Nowakowski <toalak@gmail.com>
2024-03-17 16:55:31 +0100

    Kata 2: test for single-number strings

--- katas/katas/kata_2_test.py
+++ katas/katas/kata_2_test.py
@@ -11,7 +11,17 @@ def test__should_return_zero__when_string_is_empty():
     calc = Calculator()
 
     # Act
-    result = Calculator.add("")
+    result = calc.add("")
 
     # Assert
     assert result == 0
+
+
+def test__should_return_number__when_string_has_only_one_number():
+    calc = Calculator()
+
+    result_1 = calc.add("1")
+    result_2 = calc.add("54")
+
+    assert result_1 == 1
+    assert result_2 == 54
