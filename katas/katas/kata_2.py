7d820ccf060bc27061f16289bcaab05dbfa6aad7
Tomasz Nowakowski <toalak@gmail.com>
2024-03-17 16:50:37 +0100

    Kata 2: Made test pass for empty string

---
+++ katas/katas/kata_2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+class Calculator:
+    @staticmethod
+    def add(exprstr):
+        if exprstr == "":
+            return 0
--- katas/katas/kata_2_test.py
+++ katas/katas/kata_2_test.py
@@ -3,6 +3,8 @@
 
 import pytest
 
+from kata_2 import Calculator
+
 
 def test__should_return_zero__when_string_is_empty():
     # Arrange
