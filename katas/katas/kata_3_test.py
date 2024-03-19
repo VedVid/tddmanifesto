8e7327c3d5cc30034d1a185cee59697a2bafe63a
Tomasz Nowakowski <toalak@gmail.com>
2024-03-19 21:10:54 +0100

    Add new case for multiple-bytes-long characters

--- katas/katas/kata_3_test.py
+++ katas/katas/kata_3_test.py
@@ -11,6 +11,8 @@ def test__should_return_error__when_password_is_shorter_than_8_characters():
 
     result_1 = validator.valide("")
     result_2 = validator.valide("1234567")
+    result_3 = validator.valide("你好你好你好")
 
     assert result_1 == "Password must be at least 8 characters"
     assert result_2 == "Password must be at least 8 characters"
+    assert result_3 == "Password must be at least 8 characters"
