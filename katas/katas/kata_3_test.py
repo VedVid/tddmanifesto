d33d0508ca33a1f9a810d7e7710984fce4c86ecc
Tomasz Nowakowski <toalak@gmail.com>
2024-03-19 21:14:16 +0100

    Kata 3: failing test for ensuring that there are at least two numbers in password

--- katas/katas/kata_3_test.py
+++ katas/katas/kata_3_test.py
@@ -16,3 +16,11 @@ def test__should_return_error__when_password_is_shorter_than_8_characters():
     assert result_1 == "Password must be at least 8 characters"
     assert result_2 == "Password must be at least 8 characters"
     assert result_3 == "Password must be at least 8 characters"
+
+
+def test__should_return_error__when_password_lacks_two_numbers():
+    validator = Validator()
+
+    result = validator.valide("abcdefgh")
+
+    assert result == "The password must contain at least 2 numbers"
