d635fa97bcef5a776ce8175ba0ee28b31a4353ae
Tomasz Nowakowski <toalak@gmail.com>
2024-03-19 21:36:51 +0100

    Kata 3: make the tests pass

--- katas/katas/kata_3.py
+++ katas/katas/kata_3.py
@@ -9,4 +9,6 @@ class Validator:
             errors.append("Password must be at least 8 characters")
         if sum(ch.isdigit() for ch in password) < 2:
             errors.append("The password must contain at least 2 numbers")
+        if sum(ch.isupper() for ch in password) < 1:
+            errors.append("The password must contain at least one capital letter")
         return errors
