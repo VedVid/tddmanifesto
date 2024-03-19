59c563486dbd0c18aed4049124af816094cd3f53
Tomasz Nowakowski <toalak@gmail.com>
2024-03-19 21:17:05 +0100

    Kata 3: make the tests pass; refactoring is not necesarry

--- katas/katas/kata_3.py
+++ katas/katas/kata_3.py
@@ -6,3 +6,5 @@ class Validator:
     def valide(password):
         if len(password) < 8:
             return "Password must be at least 8 characters"
+        if sum(ch.isdigit() for ch in password) < 2:
+            return "The password must contain at least 2 numbers"
