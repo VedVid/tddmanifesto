fa8f0fc9740d26091074da3cac600e60119ae786
Tomasz Nowakowski <toalak@gmail.com>
2024-03-23 22:12:24 +0100

    Kata 4: failing tests to return something

--- katas/katas/kata_4_test.py
+++ katas/katas/kata_4_test.py
@@ -12,3 +12,11 @@ def test__should_return_none__when_search_text_is_shorter_than_2_character():
     result = city_finder.find("a")
 
     assert result is None
+
+
+def test__should_return_something__when_seaerch_text_is_longer_or_equal_to_two_characters():
+    city_finder = CityFinder()
+
+    result = city_finder.find("aa")
+
+    assert result is not None
