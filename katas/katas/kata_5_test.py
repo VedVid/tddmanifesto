e543f7876b5b76f939d57a7a7360160846a80b0a
Tomasz Nowakowski <toalak@gmail.com>
2024-03-23 22:45:58 +0100

    Kata 5: failing test for 9999

--- katas/katas/kata_5_test.py
+++ katas/katas/kata_5_test.py
@@ -20,3 +20,11 @@ def test__should_return_correct_price__when_23456_barcode_given():
     result = barcode_scanner.scan("23456")
 
     assert result == "$12.50"
+
+
+def test__should_return_error__when_9999_barcode_given():
+    barcode_scanner = BarcodeScanner()
+
+    result = barcode_scanner.scan("9999")
+
+    assert result == "Error: barcode not found"
