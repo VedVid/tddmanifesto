2895ad0f13be203575f958e5eff1015237aed1a0
Tomasz Nowakowski <toalak@gmail.com>
2024-03-23 22:10:20 +0100

    Kata 4: make the tests pass; no refactoring needed this time

--- katas/katas/kata_4.py
+++ katas/katas/kata_4.py
@@ -6,3 +6,7 @@ class CityFinder:
         "Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney",
         "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"
     ]
+
+    def find(self, searchtxt):
+        if len(searchtxt) < 2:
+            return None
