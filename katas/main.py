7fcfb71276f552c0905324cd4baa222781eb2052
Tomasz Nowakowski <toalak@gmail.com>
2024-03-24 12:28:18 +0100

    Use katas: 3, 4, 5, in main script

--- katas/main.py
+++ katas/main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-from katas import kata_1, kata_2
+from katas import kata_1, kata_2, kata_3, kata_4, kata_5
 
 
 if __name__ == "__main__":
@@ -12,5 +12,21 @@ if __name__ == "__main__":
     print()
     string_calculator = kata_2.Calculator()
     print("Kata 2:")
-    print('Result of "//sep\ n2sep5"')
+    print(r'Result of "//sep\n2sep5"')
     print(string_calculator.add("//sep\n2sep5"))
+    print()
+    password_validator = kata_3.Validator()
+    print("Kata 3:")
+    print('Validate password "password":')
+    print(password_validator.valide("password"))
+    print()
+    city_finder = kata_4.CityFinder()
+    print("Kata 4:")
+    print('Search for cities with "ri":')
+    print(city_finder.find("ri"))
+    print()
+    barcode_scanner = kata_5.BarcodeScanner()
+    print("Kata 5:")
+    print('Result of barcode "12345":')
+    print(barcode_scanner.scan("12345"))
+    print()
